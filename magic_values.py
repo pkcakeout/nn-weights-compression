5d10c62e5264d78d744b7e4ad49c475f53119af0
Paul K. Gerke <pkcakeout@gmail.com>
2018-10-11 16:33:46 +0200

    First draft

---
+++ magic_values.py
@@ -0,0 +1,9 @@
+FLOAT_WIDTH = 4 # struct and numpy interactions are still hardcoded!
+
+FLOAT_DETECT_CHAIN_LENGTH = 20
+FLOAT_DETECT_ABS_HIGH_LIMIT = 10
+
+FLOAT_DETECT_THRESHOLD = 0.5
+FLOAT_UNDETECT_THRESHOLD = 0.125
+
+MAGIC_MARKER = b"A34dfS#12" # Checked this against the example file. Must be auto generated
